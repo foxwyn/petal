af143a24a609fc88e41e1cdba37143ea3c4e6057
Foxwyn <3777609+foxwyn@users.noreply.github.com>
2023-03-26 19:40:57 -0400

    move startup messages into ready event

--- src/bot.rb
+++ src/bot.rb
@@ -27,9 +27,6 @@ module Petal
   load_modules(:Commands, 'commands')
   load_modules(:Events, 'events')
 
-  puts "Petal v#{Petal::VERSION} is now running."
-  puts "Invite at https://discord.com/api/oauth2/authorize?client_id=#{ENV['DISCORD_CLIENT_ID']}&permissions=0&scope=bot"
-  
   # Blast off!
   BOT.run
 end
---
+++ src/modules/events/ready.rb
@@ -0,0 +1,10 @@
+module Petal::Events
+  module Ready
+    extend Discordrb::EventContainer
+
+    ready do |event|
+      puts "Petal v#{Petal::VERSION} is now running"
+      puts "Invite at https://discord.com/api/oauth2/authorize?client_id=#{ENV['DISCORD_CLIENT_ID']}&permissions=0&scope=bot"
+    end
+  end
+end
